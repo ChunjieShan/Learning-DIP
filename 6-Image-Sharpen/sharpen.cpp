d9533e94af9ab5904a1e9d0c29ed1e47109e9d02
ChunjieShan <862781744@qq.com>
2022-03-09 23:11:03 +0800

    Image sharpen

---
+++ 6-Image-Sharpen/sharpen.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 单淳劼 on 2022/3/9.
+//
+
+#include "sharpen.h"
---
+++ 6-Image-Sharpen/sharpen.h
@@ -0,0 +1,19 @@
+//
+// Created by 单淳劼 on 2022/3/9.
+//
+
+#ifndef SOBEL_SHARPEN_H
+#define SOBEL_SHARPEN_H
+
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+class Sharpen
+{
+public:
+    Sharpen();
+
+};
+
+
+#endif //SOBEL_SHARPEN_H
